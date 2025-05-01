7218c3d4048e420c0278905756e4b10134301815
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-05-01 15:54:12 +1000

    feat(daisyUI): add theme controller

--- .next/static/chunks/src_pages_index_5771e187._.js
+++ .next/static/chunks/src_pages_index_5771e187._.js
@@ -4,7 +4,7 @@
   "static/chunks/node_modules_next_dist_f1d2d383._.js",
   "static/chunks/node_modules_react-dom_82bb97c6._.js",
   "static/chunks/node_modules_2a5cf4bb._.js",
-  "static/chunks/[root-of-the-server]__23b907f9._.js"
+  "static/chunks/[root-of-the-server]__7e425ce4._.js"
 ],
     source: "entry"
 });
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,14 @@
+import Mode from "./components/Mode";
+
 export default function Home() {
   return (
-    <p className="text-red-500">hello</p>
-  );
+    <>
+      <section className="w-full h-screen">
+        <section className="w-full h-[24rem]">
+          <span>Vivienne</span>
+          <Mode />
+        </section>
+      </section>
+    </>
+  )
 }
