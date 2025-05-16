9acbc3fc74705dc1ff444b348daa6ee9ce6c0462
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-05-16 14:35:10 +1000

    chore: add vercel domian portfolio-vivienne0528s-projects.vercel.app  in reCAPTCHA

--- .next/static/development/_buildManifest.js
+++ .next/static/development/_buildManifest.js
@@ -1,1 +1,22 @@
-self.__BUILD_MANIFEST = {"__rewrites":{"afterFiles":[],"beforeFiles":[],"fallback":[]},"/":["static/chunks/pages/index.js"],"/_app":["static/chunks/pages/_app.js"],"/_error":["static/chunks/pages/_error.js"],"/api/contact":["static/chunks/pages/api/contact.js"],"/api/hello":["static/chunks/pages/api/hello.js"],"/utils/SplitText":["static/chunks/pages/utils/SplitText.js"],"/utils/UseContextProvider":["static/chunks/pages/utils/UseContextProvider.js"],"sortedPages":["/","/_app","/_error","/api/contact","/api/hello","/utils/SplitText","/utils/UseContextProvider"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
+self.__BUILD_MANIFEST = {
+  __rewrites: { afterFiles: [], beforeFiles: [], fallback: [] },
+  "/": ["static/chunks/pages/index.js"],
+  "/_app": ["static/chunks/pages/_app.js"],
+  "/_error": ["static/chunks/pages/_error.js"],
+  "/api/contact": ["static/chunks/pages/api/contact.js"],
+  "/api/hello": ["static/chunks/pages/api/hello.js"],
+  "/utils/SplitText": ["static/chunks/pages/utils/SplitText.js"],
+  "/utils/UseContextProvider": [
+    "static/chunks/pages/utils/UseContextProvider.js",
+  ],
+  sortedPages: [
+    "/",
+    "/_app",
+    "/_error",
+    "/api/contact",
+    "/api/hello",
+    "/utils/SplitText",
+    "/utils/UseContextProvider",
+  ],
+};
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
