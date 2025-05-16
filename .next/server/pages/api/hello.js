178735b3f0bf286a40f262a09ea70b8b3c1bb8d6
YuweiGu98 <vivienne0528.gu@gmail.com>
2025-05-16 13:34:47 +1000

    fix(tsx): type error

--- .next/server/pages/api/hello.js
+++ .next/server/pages/api/hello.js
@@ -1,15 +1,1 @@
-const CHUNK_PUBLIC_PATH = "server/pages/api/hello.js";
-const runtime = require("../../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_bf671b14._.js");
-runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__93ade7bd._.js");
-runtime.loadChunk("server/chunks/ssr/src_pages__app_tsx_f6a248e3._.js");
-runtime.loadChunk("server/chunks/ssr/node_modules_c8cf4204._.js");
-runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__c88ca7e7._.js");
-runtime.getOrInstantiateRuntimeModule(
-  '[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => "[project]/src/pages/index.tsx [ssr] (ecmascript)", INNER_DOCUMENT => "[project]/src/pages/_document.tsx [ssr] (ecmascript)", INNER_APP => "[project]/src/pages/_app.tsx [ssr] (ecmascript)" } [ssr] (ecmascript)',
-  CHUNK_PUBLIC_PATH,
-);
-module.exports = runtime.getOrInstantiateRuntimeModule(
-  '[project]/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => "[project]/src/pages/index.tsx [ssr] (ecmascript)", INNER_DOCUMENT => "[project]/src/pages/_document.tsx [ssr] (ecmascript)", INNER_APP => "[project]/src/pages/_app.tsx [ssr] (ecmascript)" } [ssr] (ecmascript)',
-  CHUNK_PUBLIC_PATH,
-).exports;
+"use strict";(()=>{var e={};e.id=1,e.ids=[1],e.modules={1196:(e,n,t)=>{t.r(n),t.d(n,{config:()=>d,default:()=>P,routeModule:()=>f});var r={};t.r(r),t.d(r,{default:()=>a});var i=t(3480),o=t(8667),u=t(6435);function a(e,n){n.status(200).json({name:"John Doe"})}let P=(0,u.M)(r,"default"),d=(0,u.M)(r,"config"),f=new i.PagesAPIRouteModule({definition:{kind:o.A.PAGES_API,page:"/api/hello",pathname:"/api/hello",bundlePath:"",filename:""},userland:r})},3480:(e,n,t)=>{e.exports=t(5600)},5600:e=>{e.exports=require("next/dist/compiled/next-server/pages-api.runtime.prod.js")},6435:(e,n)=>{Object.defineProperty(n,"M",{enumerable:!0,get:function(){return function e(n,t){return t in n?n[t]:"then"in n&&"function"==typeof n.then?n.then(n=>e(n,t)):"function"==typeof n&&"default"===t?n:void 0}}})},8667:(e,n)=>{Object.defineProperty(n,"A",{enumerable:!0,get:function(){return t}});var t=function(e){return e.PAGES="PAGES",e.PAGES_API="PAGES_API",e.APP_PAGE="APP_PAGE",e.APP_ROUTE="APP_ROUTE",e.IMAGE="IMAGE",e}({})}};var n=require("../../webpack-api-runtime.js");n.C(e);var t=n(n.s=1196);module.exports=t})();
---
+++ src/pages/components/GetInTouch.jsx
@@ -0,0 +1,104 @@
+import React, { useContext, useEffect } from "react";
+import { useRefStore } from "../utils/useRefStore";
+import { useAuth } from "../utils/useAuth";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import ReCAPTCHA from "react-google-recaptcha";
+
+const GetInTouch = () => {
+  const { contactRef } = useContext(useRefStore);
+  const {
+    schema,
+    messageSubmit,
+    successMessage,
+    setSuccessMessage,
+    errorMessage,
+    onChange,
+    captchaToken,
+    isCoolDown,
+    coolDownMessage,
+    setCaptchaToken
+  } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+  useEffect(() => {
+    if (!captchaToken) {
+      setSuccessMessage("");
+    }
+  });
+
+  return (
+    <section
+      ref={contactRef}
+      className="bg-[#FBD5A5] text-[#001E29] flex flex-col items-center gap-4 mt-50 p-4  w-full rounded-xl"
+    >
+      <div className="text-xl md:text-2xl leading-[1.75rem] md:leading-[2rem]">
+        Connect with me
+      </div>
+      <div className="bg-gradient-to-r from-amber-500 to-orange-500 bg-clip-text text-transparent text-3xl md:text-4xl font-bold leading-[2.25rem] md:leading-[2.5rem]">
+        Get in touch
+      </div>
+
+      <form
+        onSubmit={handleSubmit(messageSubmit)}
+        className="gap-5 flex flex-col items-center text-[16px] md:text-xl leading-[1.5rem] md:leading-[1.75rem]"
+      >
+        <p className="text-center">
+          {
+            "I'd love to hear from you! If you have any questions, comments or feedback, please use the form below."
+          }
+        </p>
+        <div className="flex gap-5 h-10 w-full">
+          <input
+            type="text"
+            placeholder="Enter your name"
+            {...register("name", { required: true })}
+            className="input bg-white text-[#001E29] rounded-[5px] w-full"
+          />
+          <input
+            id="email"
+            type="email"
+            placeholder="Enter your email"
+            {...register("email", { required: true })}
+            className="input bg-white text-[#001E29] rounded-[5px] w-full"
+          />
+        </div>
+        <textarea
+          className="textarea bg-white text-[#001E29] rounded-[5px] w-full h-50"
+          placeholder="Enter your message"
+          {...register("message", { required: true })}
+        ></textarea>
+        <div className="text-red-600 font-extrabold text-[14px] flex flex-col justify-center items-center">
+          <p>{errors.name?.message}</p>
+          <p>{errors.email?.message}</p>
+          <p>{errors.message?.message}</p>
+          {successMessage && <p className="text-green-600">{successMessage}</p>}
+          {errorMessage && <p>{errorMessage}</p>}
+          {!captchaToken && <p> Please complete the CAPTCHA verification.</p>}
+          {isCoolDown && <p>{coolDownMessage}</p>}
+        </div>
+
+        <button
+          disabled={!captchaToken || isCoolDown}
+          id="submit"
+          className="w-full btn btn-l md:btn-xl border-0 bg-gradient-to-r from-amber-500 to-orange-500 hover:scale-105 shadow-sm hover:shadow-xl transition duration-200 ease-in-out text-[#FFFFFF] text-[16px] md:text-xl leading-[1.5rem] md:leading-[1.75rem] rounded-[50px] md:w-50"
+        >
+          Send to me →
+        </button>
+        <ReCAPTCHA
+          sitekey="6LcFozcrAAAAAB9nUS1WeuilEhBld64y9gqmkHV7"
+          onChange={onChange}
+          onExpired={() => setCaptchaToken(null)}
+        />
+      </form>
+    </section>
+  );
+};
+
+export default GetInTouch;
